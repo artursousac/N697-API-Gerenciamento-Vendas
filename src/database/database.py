1e4e35cd0f161a1b1b6681d66b79cfa21ff45098
artursousac <artursousac@hotmail.com>
2024-09-14 21:07:34 -0300

    adicionando provaveis bibliotecas de uso

---
+++ src/database/database.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+uri = f"mongodb+srv://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASS")}@cluster0.brp8cuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri)
+
+db = client.GerenciamentoDeEventos
+
---
+++ src/model/models.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional, List
+
