32aa90304a09d80b9f5bd591e44821795389a48b
artursousac <artursousac@hotmail.com>
2024-09-15 09:49:35 -0300

    testando rota para deploy

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -2,3 +2,7 @@ from fastapi import APIRouter
 from bson import ObjectId
 
 router = APIRouter()
+
+@router.get("/")
+async def RotaInicial():
+    return {"message": "bem-vindo à primeira rota do projeto"}
